783d69d8e267e1bef8edab4ef7f3dcc35e3a8b4b
kody <kody@transverbapp.com>
2025-11-21 16:30:32 -0500

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material'
+import FlexBetween from './FlexBetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import profileImage from "assets/profile_picture.png"
+import { AppBar, Toolbar, useTheme } from '@mui/material';
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  return <AppBar
+    sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+    }}
+    >
+        <Toolbar sx={{ justifyContent: "space-between"}}>
+            {/* Left Side */}
+            <FlexBetween>
+                <IconButton onClick={() => console.log('open/close sidebar')}>
+                    <MenuIcon />
+                </IconButton>
+                <FlexBetween
+                    backgroundColor ={theme.palette.background.alt}
+                    borderRadis="9px"
+                    gap="3rem"
+                    p="0.1rem 1.5rem" >
+
+                  <InputBase placeholder="Search..." />
+                  <IconButton>
+                    <Search />
+                  </IconButton>
+                </FlexBetween>
+                </FlexBetween>
+        </Toolbar>
+    </AppBar>
+}
+
+export default Navbar
